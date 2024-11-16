afcdb9cfc056b208b7871d34d002448340ae9930
Felipe Leitao <vieira.felipe@gmail.com>
2024-11-16 11:16:31 -0300

    Projeto Um

---
+++ src/main/java/com/devmaster/fjedi90/Fjedi90Application.java
@@ -0,0 +1,13 @@
+package com.devmaster.fjedi90;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fjedi90Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fjedi90Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/devmaster/fjedi90/Fjedi90ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmaster.fjedi90;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fjedi90ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
